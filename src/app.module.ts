60ff9a67e88b0e8658df60723905d66a5bcc2b05
QuangDev <ng.quang.coder.new@gmail.com>
2025-09-10 14:57:49 +0700

    fix lỗi : [Error] : Query root type must be provided và chạy thử chức năng GraphQL playground qua đường dẫn http://localhost:3000/graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      
     }),
   ],
+  providers: [AppResolver],
 })
 export class AppModule {}
